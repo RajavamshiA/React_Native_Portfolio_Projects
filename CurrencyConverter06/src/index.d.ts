603294a02cad65a41e1887fa40fe4eed7bba7f3e
RajavamshiA <75177605+RajavamshiA@users.noreply.github.com>
2023-11-01 21:26:57 +0000

    Adding React Native files from local repository to github

---
+++ CurrencyConverter06/src/index.d.ts
@@ -0,0 +1,8 @@
+
+//Whenever creating a button, we follow this interface and these properties are must to have in that.
+interface Currency{
+    name:string;
+    value:number;
+    flag:string;
+    symbol:string;
+}
